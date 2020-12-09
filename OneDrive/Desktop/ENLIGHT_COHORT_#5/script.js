0ee54900d874d4eb7cf1288d6b6b27d0ac1aa890
brian gebara <bgebara@umich.edu>
2020-12-08 20:47:49 -0500

    first website

---
+++ OneDrive/Desktop/ENLIGHT_COHORT_#5/script.js
@@ -0,0 +1,8 @@
+document.body.onkeyup = function (event) {
+  console.log(event.keyCode);
+  if (event.keyCode == 68) {
+    // change the class of body to dark mode
+    document.body.classList.toggle("dark");
+
+  }
+}
